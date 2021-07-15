75b6cacf209a159db825876752fa5b120abead11
V Kon <36918300+vakonyakhin@users.noreply.github.com>
2021-07-15 15:26:28 +0000

    create app

--- main.py
+++ main.py
@@ -1,58 +1,95 @@
-from datetime import date
+from datetime import date, timedelta
 import requests
 import os
 import time
+import random
+from pathlib import Path
 
 import telegram
 from telegram.ext import Updater, Dispatcher, MessageHandler, CommandHandler
 from telegram.ext import CallbackContext
 from telegram.ext import Filters
 
+
 def start(update, context):
     update.message.reply_text('Поехали. Буду присылать по одной фотографии каждый день')
-    
+
 
 def text(update, context):
-  update.message.reply_text('Я не понимаю текст и не веду беседу. Завтра будет новое фото дня от NASA')
+    update.message.reply_text('Я не понимаю текст и не веду беседу. Завтра будет новое фото.')
 
 
-def get_nasa_foto(url, key):
+def download_image(url, name, directory='images/'):
+    response = requests.get(url)
+    response.raise_for_status
+
+    image = Path(f'{directory}{name}')
+    if not image.exists():
+        with open(f'{directory}{name}', 'wb') as image:
+            image.write(response.content)
+
+
+def get_nasa_images(url, key):
+  delta = 30
+  time_delta = timedelta(days=delta)
   params = {
       'api_key': key,
-      'date': date.today(),
+      'start_date': date.today() - time_delta,
+      'end_date': date.today(),
   }
   response = requests.get(url, params=params)
   response.raise_for_status()
 
-  return response.json()['url']
+  nasa_content = response.json()
+  return nasa_content
 
 
-def post_image(update,chat_id, photo):
-    update.bot.send_photo(chat_id, photo)
+def find_image(directory='images/'):
+    images = os.listdir(directory)
+    
+    for image_num,image in enumerate(images):
+        with open(directory + image, 'rb') as image:
+            return image.read()
 
+
+def post_image(update, chat_id, image):
+    update.bot.send_photo(chat_id, image)
 
 def main():
+
     chat_id = os.getenv('CHAT_ID')
     tg_token = os.getenv('TG_KEY')
-    nasa_url = 'https://api.nasa.gov/planetary/apod'
+
     nasa_key = os.getenv('NASA_API_KEY')
+    nasa_url = 'https://api.nasa.gov/planetary/apod'
+    name_nasa = '_nasa.jpg'
+
+    name_spacex = '_spaceX.jpg'
+    launch_spacex = random.randint(1, 100)
+    spacex_url = f'https://api.spacexdata.com/v3/launches/{launch_spacex}'
+    directory = 'images/'
+
     updater = Updater(tg_token, use_context=True)
-
     dispatcher = updater.dispatcher
-
     dispatcher.add_handler(CommandHandler('start', start))
     dispatcher.add_handler(MessageHandler(Filters.text & (~Filters.command), text))
 
-    image = get_nasa_foto(nasa_url, nasa_key)
-    
-    post_image(updater,chat_id, image)
-    
- 
-if __name__ == '__main__':
-  while True:
-    main()
-    time.sleep(86400)
+    if not os.path.exists(directory):
+       os.mkdir(directory)
 
+    nasa_content = get_nasa_images(nasa_url, nasa_key)
+    for image in nasa_content:
+        download_image(image['url'], f'{image["date"]}{name_nasa}')
+
+    while True:
+        image = find_image()
+        post_image(updater, chat_id, image)
+        time.sleep(10)
+
+if __name__ == '__main__':
+    main()
+  
+  
 
 
 
